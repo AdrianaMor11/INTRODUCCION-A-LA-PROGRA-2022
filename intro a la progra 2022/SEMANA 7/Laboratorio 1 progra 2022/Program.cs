95b849826bb1f17ea2efef9f461918265bd05e6e
AdrianaMor11 <111014526+AdrianaMor11@users.noreply.github.com>
2022-09-21 18:30:22 -0600

    Add files via upload

---
+++ intro a la progra 2022/SEMANA 7/Laboratorio 1 progra 2022/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Laboratorio_AMO_1057021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre: ");
+            string Nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola mundo!");
+            Console.WriteLine("Soy Adriana Morales");
+
+            /*Al comparar console.writeline con console.write podemos notar que al ejecutarlos console.writeline coloca las oraciones de manera separada y ordenada, mientras que console.write no deja espacio y se ve de manera mas desordenada*/
+
+            Console.Write("Hola mundo");
+            Console.Write("Soy Adriana Morales");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ intro a la progra 2022/SEMANA 7/T1_AMO_1057021/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T1_AMO_1057021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa!");
+            Console.WriteLine("Ingrese su nombre: ");
+                string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad: "); 
+                string edad = Console.ReadLine();
+            Console.WriteLine("Ingrese su carrera: ");
+                string carrera = Console.ReadLine();
+            Console.WriteLine("Ingrese su carne: ");
+            string carne = Console.ReadLine();
+
+            Console.Write(" Soy " + nombre);
+            Console.Write(" tengo " + edad);
+            Console.Write(" años y estudio la carrera de " + carrera);
+            Console.Write(". Mi número de carné es " + carne);
+            Console.ReadKey();
+        }
+    }
+}
