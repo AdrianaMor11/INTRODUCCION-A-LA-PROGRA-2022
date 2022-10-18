d53f542ad599a13f67bc417ec4c9f28e53001b20
AdrianaMor11 <111014526+AdrianaMor11@users.noreply.github.com>
2022-10-17 22:23:54 -0600

    Add files via upload

---
+++ intro a la progra 2022/Tarea8_1057021/T8/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcularBinario(object sender, MouseEventArgs e)
+        {
+            var numero = int.Parse(decimalBi.Text);
+            long bin = 0;
+
+            const int DIVISOR = 2;
+            long digito = 0;
+
+            for (int i = numero % DIVISOR, j = 0; numero > 0; numero /= DIVISOR, i = numero % DIVISOR, j++)
+            {
+                digito = i % DIVISOR;
+                bin += digito * (long)Math.Pow(10, j);
+            }
+
+            binario.Text = bin.ToString();
+        }
+
+        private void calcularHexadecimal(object sender, MouseEventArgs e)
+        {
+            var numero = int.Parse(decimalHexa.Text);
+
+            hexadecimal.Text = Convert.ToString(numero, 16).ToUpper();
+        }
+    }
+}
