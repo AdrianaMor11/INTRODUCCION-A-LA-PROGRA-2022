d4446444225063f4287159fd9dbda9a9750fe66a
AdrianaMor11 <111014526+AdrianaMor11@users.noreply.github.com>
2022-10-23 18:46:24 -0600

    Add files via upload

---
+++ intro a la progra 2022/SEMANA 10/laboratorio10/laboratorio10/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int contador = 1;
+string usuario = "";
+string pass = "";
+
+while(contador <= 3)
+{
+    Console.WriteLine("Ingrese Usuario");
+    usuario = Console.ReadLine().ToString();
+    Console.WriteLine("Ingrese Contraseña");
+    pass = Console.ReadLine().ToString();
+
+    if(usuario.Equals("usuario1") && pass.Equals("asdasd"))
+    {
+        Console.WriteLine("Verdadero");
+        break;
+    }
+    else
+    {
+        contador++;
+    }
+}
+
+if(contador == 3)
+{
+    Console.WriteLine("Cantidad de intentos permitidos superado");
+}
