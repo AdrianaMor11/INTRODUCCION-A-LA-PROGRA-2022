2cfeebd1babbb0939065b2d57bb877f42d4abd2b
AdrianaMor11 <111014526+AdrianaMor11@users.noreply.github.com>
2022-10-23 18:53:42 -0600

    Add files via upload

---
+++ intro a la progra 2022/SEMANA 11/RETO 2/RETO 2 SEMANA 11.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace reto02semana11_1057021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HECHO POR ADRIANA MORALES 1057021, MARCELO RAMAZZINI 1250521, MAURICIO CONEDERA 1253303");
+            int[] nivelesA = new int[5];
+            int[] nivelesN = new int[5];
+            int suma = 0;
+            int suma2 = 0;
+            int suma3 = 0;
+            int contador = 1;
+            int contador2 = 1;
+            int contador3 = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("ingrese la cantidad de adultos en el nivel " + contador);
+                nivelesA[i] = int.Parse(Console.ReadLine());
+                contador++;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("ingrese la cantidad de niños en el nivel " + contador2);
+                nivelesN[i] = int.Parse(Console.ReadLine());
+                contador2++;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                suma += nivelesA[i];
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                suma2 += nivelesN[i];
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                suma3 = nivelesA[i] + nivelesN[i];
+                Console.WriteLine("la suma de las personas en el nivel " + contador3 + " es de:" + suma3);
+                contador3++;
+            }
+            Console.WriteLine("la suma de los adultos en el edificio es: " + suma);
+            Console.WriteLine("la suma de los niñso en el edificio es: " + suma2);
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ intro a la progra 2022/SEMANA 11/RETO 3/RETO 3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace reto03semana11_1057021
+{
+    class Program
+    {
+
+        public string[] niveles; //Declaramos un vector de tipo string para los niveles
+        public float[] personas; //Declaramos un vector de tipo float para las personas
+
+        public void Cargar()
+        {
+            niveles = new string[5];
+            personas = new float[5];
+            for (int f = 0; f < niveles.Length; f++)
+            {
+                Console.Write("Ingrese el nivel: " + (f + 1) + ": ");
+                niveles[f] = Console.ReadLine();
+                Console.Write("Ingrese el número de personas " + (f + 1) + ": ");
+                string linea;
+                linea = Console.ReadLine();
+                personas[f] = float.Parse(linea);
+            }
+        }
+
+        public void Mayorpersonas()
+        {
+            float mayor;
+            int pos;
+            mayor = personas[0];
+            pos = 0;
+            for (int f = 1; f < niveles.Length; f++)
+            {
+                if (personas[f] > mayor)
+                {
+                    mayor = personas[f];
+                    pos = f;
+                }
+            }
+            Console.WriteLine("El empleado con sueldo mayor es :" + niveles[pos]);
+            Console.WriteLine("Tiene un sueldo de: " + mayor);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HECHO POR ADRIANA MORALES 1057021, MARCELO RAMAZZINI 1250521, MAURICIO CONEDERA 1253303");
+        }
+    }
+}
